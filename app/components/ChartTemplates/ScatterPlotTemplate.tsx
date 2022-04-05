46cc08b38041eaa13d26144aaedf8ade9972eabb
Lionel Ong <lionel.ongtzemin@gmail.com>
2022-04-05 22:04:12 +0800

    fix: scatter plot bug

--- app/components/ChartTemplates/ScatterPlotTemplate.tsx
+++ app/components/ChartTemplates/ScatterPlotTemplate.tsx
@@ -33,8 +33,12 @@ export function ScatterPlotTemplate({ data, title, xlabel, ylabel, name }: Scatt
           }}
         >
           <CartesianGrid />
-          <XAxis dataKey='xval' label={{ value: xlabel, position: 'bottom', fill: mode === 'light' ? '#000' : '#fff' }} />
-          <YAxis dataKey='yval' label={{ value: ylabel, angle: -90, position: 'left', fill: mode === 'light' ? '#000' : '#fff' }} />
+          <XAxis dataKey='xval' type='number' label={{ value: xlabel, position: 'bottom', fill: mode === 'light' ? '#000' : '#fff' }} />
+          <YAxis
+            dataKey='yval'
+            type='number'
+            label={{ value: ylabel, angle: -90, position: 'left', fill: mode === 'light' ? '#000' : '#fff' }}
+          />
           {/* Dot Size https://stackoverflow.com/questions/68437581/how-to-customize-recharts-scatter-circle-size */}
           <ZAxis range={[250, 250]} />
           <Tooltip labelStyle={{ color: 'black' }} cursor={{ strokeDasharray: '3 3' }} />
